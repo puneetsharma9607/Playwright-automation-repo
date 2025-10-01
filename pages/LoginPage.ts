963413ab4903667c3b12373fc5281fb8541b9230
Puneet Sharma <puneet.sharma9607@gmail.com>
2025-10-02 01:15:50 +0530

    Updated readme, added chai library

---
+++ pages/LoginPage.ts
@@ -0,0 +1,9 @@
+import {Locator, Page} from '@playwright/test';
+import {assert, expect, util} from 'chai';
+
+export class LoginPage {
+    privatedata:any;
+    private testdata:any;
+    private elements!: Locator;
+    
+}
---
+++ support/commonMethods.ts
@@ -0,0 +1,42 @@
+import { Page, ElementHandle, Locator } from '@playwright/test';
+
+export class CommonMethods {
+    constructor(private page: Page) {
+        this.page = page;
+    }
+
+    async waitForNetworkLoadState() {
+        try {
+
+            //Attempt to wait for network to be idle for upto 10 seconds
+            await this.page.waitForLoadState('networkidle', { timeout: 10000 });
+        }
+        catch (error: unknown) {
+            console.error('Unexpected error', error)
+
+            //Throw error
+
+        }
+    }
+
+    async waitForElement(selector: any) {
+
+        try {
+            await this.page.waitForSelector(selector, { state: 'visible' });
+        }
+        catch (error) {
+            console.error('Error interacting with the page', error)
+        }
+    }
+
+    async clickOnElement(selector: any) {
+        try {
+            await this.waitForElement(selector);
+            await this
+        }
+        catch (error) {
+            console.error('Error interacting with the page', error)
+        }
+    }
+    async
+}
